c0482ab545b684cba52ffcf257754b052fce78ef
@Donskoy <diman@arocketmail.com>
2018-04-21 06:06:38 -0500

    movie search somewhat working

--- liri.js
+++ liri.js
@@ -5,3 +5,65 @@ var twitter = require("twitter");
 var Spotify = require('node-spotify-api');
 var request = require("request");
 var fs = require('fs');
+
+// INSTRUCTIONS:
+// ---------------------------------------------------------------------------------------------------------
+// Level 1:
+// Take any movie with a word title (ex: Cinderella) as a Node argument and retrieve the year it was created
+
+// Level 2 (More Challenging):
+// Take a move with multiple words (ex: Forrest Gump) as a Node argument and retrieve the year it was created.
+// ---------------------------------------------------------------------------------------------------------
+
+// Include the request npm package (Don't forget to run "npm install request" in this folder first!)
+var request = require("request");
+
+// Store all of the arguments in an array
+var nodeArgs = process.argv;
+
+// Create an empty variable for holding the movie name
+var movieName = "";
+
+// Loop through all the words in the node argument
+// And do a little for-loop magic to handle the inclusion of "+"s
+for (var i = 2; i < nodeArgs.length; i++) {
+
+  if (i > 2 && i < nodeArgs.length) {
+
+    movieName = movieName + "+" + nodeArgs[i];
+
+  }
+
+  else {
+
+    movieName += nodeArgs[i];
+
+  }
+}
+
+// Then run a request to the OMDB API with the movie specified
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&tomatoes=true&apikey=trilogy";
+
+// This line is just to help us debug against the actual URL.
+console.log(queryUrl);
+
+request(queryUrl, function(error, response, body) {
+
+  // If the request is successful
+  if (!error && response.statusCode === 200) {
+
+    // Parse the body of the site and recover just the imdbRating
+    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+    // console.log("Release Year: " + JSON.parse(body).Year);
+
+    console.log("Title: " + JSON.parse(body).Title);
+    console.log("Year: " + JSON.parse(body).Year);
+    console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
+    console.log("Rotten Tomatoes URL: " + JSON.parse(body).tomatoURL);
+    console.log("Country: " + JSON.parse(body).Country);
+    console.log("Language: " + JSON.parse(body).Language);
+    console.log("Plot: " + JSON.parse(body).Plot);
+    console.log("Actors: " + JSON.parse(body).Actors);
+  }
+});
