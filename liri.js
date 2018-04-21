55137984481b85bfb71cc7860f587cf0f5ea30e7
@Donskoy <diman@arocketmail.com>
2018-04-21 04:04:45 -0500

    npi all files installed

--- liri.js
+++ liri.js
@@ -1,1 +1,7 @@
 require("dotenv").config();
+
+var keys = require('./keys.js');
+var twitter = require("twitter");
+var Spotify = require('node-spotify-api');
+var request = require("request");
+var fs = require('fs');
