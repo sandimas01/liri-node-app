9e5d325b0d8991436aa6e4f9d96b64902a411c12
@Donskoy <diman@rocketmail.com>
2018-04-23 15:14:59 -0500

    spotify somewhat working in separate file

--- test2.js
+++ test2.js
@@ -5,3 +5,35 @@ var Twitter = require("twitter");
 var Spotify = require('node-spotify-api');
 var request = require("request");
 var fs = require('fs');
+console.log('app started')
+
+var spotify = new Spotify(keys.spotify);
+
+var searchTerm = "for whom the bell tolls"
+
+
+spotify.search({ type: 'track', query: searchTerm, count: 10 }, function(err, data) {
+    for (var i = 0; i < 10; i++) {
+        console.log("spotify input:", searchTerm);
+        // console.log("_________________");
+        console.log("-----------------");
+        // console.log("Artist: ", data.tracks.items[i].album.artists[i].name);
+        // console.log("_________________");
+        console.log("Album: ", data.tracks.items[i].album.name);
+        // console.log("_________________");
+        console.log("Preview link: ", data.tracks.items[i].preview_url);
+        // console.log("_________________");
+        console.log("Song Name: ", data.tracks.items[i].name);
+        console.log("_________________");
+        console.log("*****************");
+        
+        if (err) {
+            console.log('Error occurred: ' + err);
+            searchTerm = "the+sign+ace+of+base";
+            return;
+        }
+    };
+
+});
+
+
